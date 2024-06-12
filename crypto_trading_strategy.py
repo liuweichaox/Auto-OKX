d3c79833c96309db83e73db5398cbeec113d6ed8
Waychao Liu <liuweichaox@gmail.com>
2024-06-13 01:17:59 +0800

    rsi

--- crypto_trading_strategy.py
+++ crypto_trading_strategy.py
@@ -468,7 +468,7 @@ class TradingStrategy:
         返回:
         bool: 是否满足买入条件。
         """
-        return df["rsi"].iloc[-1] < 99
+        return df["rsi"].iloc[-1] < 40
 
     def check_sell_condition(self, df):
         """
@@ -480,7 +480,7 @@ class TradingStrategy:
         返回:
         bool: 是否满足卖出条件。
         """
-        return df["rsi"].iloc[-1] > 11
+        return df["rsi"].iloc[-1] > 60
 
     def dynamic_take_profit_and_stop_loss(
         self,
@@ -508,10 +508,10 @@ class TradingStrategy:
         tuple: 止盈价格和止损价格。
         """
         # 基于RSI调整止盈止损价格
-        if rsi < 99:
+        if rsi < 40:
             take_profit_price = entry_price * (1 + base_take_profit_ratio + atr)
             stop_loss_price = entry_price * (1 - base_stop_loss_ratio - atr)
-        elif rsi > 11:
+        elif rsi > 60:
             take_profit_price = entry_price * (1 + base_take_profit_ratio - atr)
             stop_loss_price = entry_price * (1 - base_stop_loss_ratio + atr)
         else:
